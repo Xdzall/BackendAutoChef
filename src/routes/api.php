1c080e473946198d9dc47eacb404e3e05cd2ebfc
Ubuntu <autochef@AutoChef.botmhxttgxkuvjpib0lhjvnfqb.ix.internal.cloudapp.net>
2025-10-18 08:12:28 +0000

    verifikasi email

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+    // SEMUA RUTE DI SINI HANYA BISA DIAKSES JIKA PENGGUNA SUDAH LOGIN & TERVERIFIKASI
+    Route::get('/my-recipes', [RecipeController::class, 'index']);
+});
+\
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function() {
+    // HANYA ADMIN YANG BISA MENGAKSES RUTE INI
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index']);
 });
